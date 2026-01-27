1008a62d65ec20fdad24a0c4ca90529fac33a3ea
longvo34 <vothanhlong0987@gmail.com>
2026-01-27 19:04:26 +0700

    done register vehicle, show vehicle,detail

---
+++ src/screens/home/profile/registerVehicle/vehicleDetail/VehicleDetailScreen.styles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from "react-native";
+import COLORS from "../../../../../constants/colors";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: COLORS.white,
+  },
+  headerTitle: {
+    flex: 1,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "600",
+    color: COLORS.text,
+  },
+  image: {
+    width: "100%",
+    height: 220,
+    backgroundColor: "#eee",
+  },
+  card: {
+    backgroundColor: COLORS.white,
+    margin: 16,
+    padding: 16,
+    borderRadius: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: COLORS.text,
+    marginBottom: 8,
+  },
+  status: {
+    color: COLORS.primary,
+    fontWeight: "600",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 12,
+  },
+  label: {
+    color: COLORS.gray,
+  },
+  value: {
+    color: COLORS.text,
+    fontWeight: "500",
+  },
+});
